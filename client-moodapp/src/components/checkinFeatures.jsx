9526f588022e627d8720b5ef88267a77ae921bb2
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-06 18:40:53 +0000

    Component Container for User input Layout

---
+++ client-moodapp/src/components/checkinFeatures.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class CheckInFeature extends Component {
+  state = {};
+  render() {
+    return <h1>test</h1>;
+  }
+}
+
+export default CheckInFeature;
