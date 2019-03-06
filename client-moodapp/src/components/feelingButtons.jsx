bd045256a1f435823ca27649a460f2e4c90fd6dd
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-06 19:13:53 +0000

    components for buttons holder

---
+++ client-moodapp/src/components/feelingButtons.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const FeelingButtons = props => {
+  var btnClass = "btn btn-outline-light text-dark";
+  return (
+    <div id="feelingsbtn">
+      {props.feelings.map((x, y) => (
+        <button
+          className={x.isActive == true ? btnClass + " active" : btnClass}
+          onClick={props.onFeelingsSelected}
+          key={y}
+          id={y}
+        >
+          {x.isActive} {x.text}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default FeelingButtons;
