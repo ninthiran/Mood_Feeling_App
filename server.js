f93f45accc809ba3649ac1bb83bce80a2b723b04
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-06 18:19:03 +0000

    -first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var app = express();
+
+var port = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+app.use(cors);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
