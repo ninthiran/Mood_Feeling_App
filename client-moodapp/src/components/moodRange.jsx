62d02590284af0d732b819efa8623285c0cc13fb
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-06 19:03:12 +0000

    Adding slider and change face according to slider movement

--- client-moodapp/src/components/moodRange.jsx
+++ client-moodapp/src/components/moodRange.jsx
@@ -4,10 +4,19 @@ const MoodRangeSlider = props => {
   return (
     <Fragment>
       <div id="face">
-        <div id="mouth-box">
-          <div id="mouth" />
+        <div id="mouth-box" className={`mood_${props.sliderValue}`}>
+          <div id="mouth" className={`mood_${props.sliderValue}`} />
         </div>
       </div>
+      <input
+        className="custom-range"
+        type="range"
+        min="1"
+        max="7"
+        value={props.sliderValue}
+        onChange={props.onhandleChange}
+        step="1"
+      />
     </Fragment>
   );
 };
