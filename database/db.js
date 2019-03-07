6c17ecf6dd0bd60ee0df346a707e793a8b709f21
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-07 07:28:52 +0000

    Database changes

--- database/db.js
+++ database/db.js
@@ -8,9 +8,23 @@ let db = new sqlite3.Database(
       console.error(err.message);
       throw err;
     } else {
-      console.log("Connected to the moodinsights database.");
+      console.log("Connected to the MoodFeatures database.");
       db.run(
-        "CREATE TABLE MoodFeatures ( Entry_ID   VARCHAR (25)  PRIMARY KEY UNIQUE, mood_range INTEGER (2)   NOT NULL, depressed  BOOLEAN       NOT NULL DEFAULT (0), optimistic BOOLEAN       NOT NULL DEFAULT (0), bored      BOOLEAN       NOT NULL DEFAULT (0), happy      BOOLEAN       NOT NULL DEFAULT (0), comments   VARCHAR (250) NOT NULL DEFAULT (0), time   TIME    DEFAULT (CURRENT_TIMESTAMP)  );",
+        `CREATE TABLE MoodFeatures (
+              Entry_ID   INTEGER       PRIMARY KEY AUTOINCREMENT
+                                       UNIQUE,
+              mood_range INTEGER (2)   NOT NULL,
+              depressed  BOOLEAN       NOT NULL
+                                       DEFAULT (0),
+              optimistic BOOLEAN       NOT NULL
+                                       DEFAULT (0),
+              bored      BOOLEAN       NOT NULL
+                                       DEFAULT (0),
+              happy      BOOLEAN       NOT NULL
+                                       DEFAULT (0),
+              comments   VARCHAR (250),
+              time       TIME          DEFAULT (CURRENT_TIMESTAMP) 
+          );`,
         err => {
           console.log("Table already created");
         }
