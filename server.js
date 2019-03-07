e8320ed2b7c3b04eeb25b6fdc2f60b235ce5b6e0
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-07 07:28:22 +0000

    Adding post method to server.js

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ var bodyParser = require("body-parser");
 var app = express();
 
 var port = process.env.PORT || 3001;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var db = require("./database/db");
 
-app.get("/", function(req, res) {
+app.get("/users", (req, res) => {
   const select = "SELECT * FROM MoodFeatures";
   const params = [];
-  db.all(select, params, function(err, rows) {
+  db.all(select, params, (err, rows) => {
     if (err) {
       res.status(400).json({ error: err.message });
       return;
@@ -18,6 +20,30 @@ app.get("/", function(req, res) {
     res.json({ rows });
   });
   console.log("Sent list of items");
+});
+
+app.post("/post", (req, res) => {
+  const param = [
+    req.body.moodRangeValue,
+    req.body.depressed ? 1 : 0,
+    req.body.optimistic ? 1 : 0,
+    req.body.bored ? 1 : 0,
+    req.body.happy ? 1 : 0,
+    req.body.comments
+  ];
+
+  const insert = `INSERT INTO MoodFeatures ( mood_range, depressed, optimistic, bored, happy, comments ) 
+                values ( ${param.join(",")} )`;
+
+  console.log(insert);
+
+  db.run(insert, err => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log(`Rows inserted ${this.changes}`);
+  });
+  return res.send("Received a POST HTTP method");
 });
 
 app.listen(port, () => {
