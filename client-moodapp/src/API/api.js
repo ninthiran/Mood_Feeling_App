af7a08faa44a591dbd53e85d24e513e923538020
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-07 10:39:43 +0000

    Added post methnd in api.js

--- client-moodapp/src/API/api.js
+++ client-moodapp/src/API/api.js
@@ -3,14 +3,32 @@ import axios from "axios";
 const getDataList = (path = "") => {
   return new Promise(resolve => {
     axios
-      .get(`/`)
+      .get(`/users`)
       .then(response => {
         resolve(response);
       })
       .catch(error => {
-        console.error(error + `/api/getList`);
+        console.error(error);
       });
   });
 };
 
-export default { getDataList };
+const postDataList = state => {
+  axios
+    .post(`/post`, {
+      moodRangeValue: state.moodRangeValue,
+      depressed: state.FeelingArray[0].isActive,
+      optimistic: state.FeelingArray[1].isActive,
+      bored: state.FeelingArray[2].isActive,
+      happy: state.FeelingArray[3].isActive,
+      comments: `'${state.comments}'`
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+};
+
+export default { getDataList, postDataList };
