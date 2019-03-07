0cc624219f01f64ac9cb03e9014d335d8896d317
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-07 12:30:37 +0000

    seprate component for emoji layout which can be reused

---
+++ client-moodapp/src/components/emojiFace.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+const EmojiFace = props => {
+  console.log(props);
+  return (
+    <Fragment>
+      <div id="face">
+        <div id="mouth-box" className={`mood_${props.sliderValue}`}>
+          <div id="mouth" className={`mood_${props.sliderValue}`} />
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default EmojiFace;
