d6c3c224292e0e5d377e7431dc8f6283622333d6
ninthiran <42807808+ninthiran@users.noreply.github.com>
2019-03-07 10:39:14 +0000

    Adding new components to App.js

--- client-moodapp/src/App.js
+++ client-moodapp/src/App.js
@@ -2,14 +2,29 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import CheckInFeature from "./components/checkinFeatures";
+import InsightsContainer from "./components/insightsContainer";
+
+let postSucess = false;
 
 class App extends Component {
+  state = {
+    postSucess: false
+  };
+
+  sucessAction = () => {
+    this.setState({ postSucess: !this.state.postSucess });
+  };
+
   render() {
     return (
       <div className="App">
         <div className="container">
           <div className="main_container">
-            <CheckInFeature />
+            {this.state.postSucess ? (
+              <CheckInFeature onComplete={this.sucessAction} />
+            ) : (
+              <InsightsContainer />
+            )}
           </div>
         </div>
       </div>
